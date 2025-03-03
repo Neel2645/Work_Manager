55c61cfb2f2887ec226aa88bdf0740dce58ac53c
Neel Sathvara <sathvaraneel52@gmail.com>
2025-03-04 04:47:01 +0530

    task model created

---
+++ src/models/task.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:[true,"Title Required !!"]
+    },
+    content:{
+        type:String,
+        required:[true,"Content Required !!"]
+    },
+    status:{
+        type:String,
+        enum:["pending","completed"],
+        default:"pending"
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"users"
+    },
+    addedDate:{
+        type:Date,
+        default:Date.now(),
+    },
+    userId: {
+        type:  mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    // user: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "users",
+    //     required: true
+    // },
+
+})
+
+
+export const Task = mongoose.models.tasks || mongoose.model("tasks",TaskSchema);
