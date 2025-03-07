7edfc13f211aa3332b87b4a95b163fd1dd1820b3
use <sathvaraneel52@gmail.com>
2025-03-07 16:56:03 +0530

    some changes in navbar and footer

---
+++ src/app/showtask/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const metadata = {
+    title : "ALL Task : Work Manager"
+  }
+
+const ShowTask = () => {
+  return (
+    <div>ShowTask</div>
+  )
+}
+
+export default ShowTask
---
+++ src/components/CustomNavbar.jsx
@@ -0,0 +1,33 @@
+"use client"
+
+import Link from 'next/link'
+import React from 'react'
+
+const CustomNavbar = () => {
+  return (
+   <nav className='bg-blue-400 h-14 rounded-lg m-3 py-2 px-3 text-xl flex justify-between items-center'>
+        <div className='brand p-3'>
+            <h1 className='font-bold text-red-400'>
+                <Link href="/">Work Manager</Link>
+            </h1>
+        </div>
+
+        <div className=''>
+            <ul className='flex space-x-7'>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/addtask">AddTask</Link></li>
+                <li><Link href="/showtask">ShowTasks</Link></li>
+            </ul>
+        </div>
+
+        <div className='pr-3'>
+            <ul className='flex space-x-7 text-violet-800 font-semibold'>
+                <li><Link href="#!">Login</Link></li>
+                <li><Link href="#!">SignUp</Link></li>
+            </ul>
+        </div>
+   </nav>
+  )
+}
+
+export default CustomNavbar
